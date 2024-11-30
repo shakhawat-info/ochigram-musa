23d8821e7faa4e19a89374e48c45416da4e60ddf
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-11-30 12:48:32 +0600

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// nav start
+const NavList = document.querySelectorAll('.navIcons li');
+
+NavList.forEach((item) => {
+  item.addEventListener('click', function () {
+    NavList.forEach((deactive) => deactive.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
+// nav end
+
+
+
+let searchForm = document.querySelector('.searchForm');
+searchForm.addEventListener('click' , (event)=>{
+  searchForm.classList.add('FormExtend');
+  event.stopPropagation();
+})
+
+document.querySelector('body').addEventListener('click' , ()=>{
+  searchForm.classList.remove('FormExtend');
+
+})
