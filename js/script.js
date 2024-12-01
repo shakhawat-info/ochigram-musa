12e77036f33cb7928d79b1d9a8c3ce6e8db41708
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-12-01 15:44:31 +0600

    Poster box Done

--- js/script.js
+++ js/script.js
@@ -9,9 +9,6 @@ NavList.forEach((item) => {
 });
 // nav end
 
-
-
-
 let searchForm = document.querySelector('.searchForm');
 searchForm.addEventListener('click' , (event)=>{
   searchForm.classList.add('FormExtend');
@@ -22,3 +19,24 @@ document.querySelector('body').addEventListener('click' , ()=>{
   searchForm.classList.remove('FormExtend');
 
 })
+
+
+
+// poster start
+
+let postbtn = document.querySelector('.postbtn');
+let postText = document.querySelector('#postText')
+function post(){
+  if(postText.value){
+    postbtn.style.background = 'rgb(255, 4, 92)'
+    postbtn.style.color = '#fff'
+  }
+  else{
+    postbtn.style.background = 'rgba(101, 104, 108, 0.2)'
+    postbtn.style.color = '#000'
+  }
+}
+
+
+
+// poster end
