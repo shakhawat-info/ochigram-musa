ac563067f5101a922b4ab292ce251a1f138510f5
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-12-06 15:06:39 +0600

    FootNav Update

--- js/script.js
+++ js/script.js
@@ -70,40 +70,29 @@ posterClose.addEventListener('click' , ()=>{
 
 
 
-$('.stories').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 11,
-  slidesToScroll: 1,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});	
+// footer start
+let footNav = document.querySelector('.footNav');
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+
+  let currentScrollTop = window.scrollY 
+
+  if (currentScrollTop > lastScrollTop) {
+
+    footNav.classList.add('footNavHide')
+
+  } else if (currentScrollTop < lastScrollTop) {
+    
+    footNav.classList.remove('footNavHide')
+
+  }
+
+
+  lastScrollTop = currentScrollTop;
+});
+
+// footer end
+
 	
